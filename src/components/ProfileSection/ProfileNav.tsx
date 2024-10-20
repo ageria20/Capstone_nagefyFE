b3149b911d0e868d5ccac2bc934716fe9ba56dee
ageria20 <ageria20@gmail.com>
2024-10-20 16:05:44 +0200

    first step for home page

--- src/components/ProfileSection/ProfileNav.tsx
+++ src/components/ProfileSection/ProfileNav.tsx
@@ -2,13 +2,14 @@
 import { Button, Container, Image, Nav, Navbar } from 'react-bootstrap'
 import { useNavigate } from 'react-router-dom'
 import nagefyLogo from "../../assets/nagefyLogo250.png"
+import "./ProfileSection.css"
 
 const ProfileNav = () => {
 const navigate = useNavigate()
 
   return (
-    <Navbar expand="lg" className="navbar m-0 p-0 ">
-      <Container className="">
+    <Navbar expand="lg" className="navbar  mx-auto p-0">
+      <Container className="bg-white rounded-4 mb-5">
         <Navbar.Brand onClick={() => navigate("/")}>
           <Image src={nagefyLogo} alt="nagefy_logo" width={120} />
         </Navbar.Brand>
@@ -18,26 +19,26 @@ const navigate = useNavigate()
             <Nav.Link
               className="navbarMenu mx-3"
               onClick={() => navigate("/")}
-              style={{ fontSize: "1.2rem" }}
+              style={{ fontSize: "1.2rem", fontWeight: "300" }}
             >
               Home
             </Nav.Link>
             
-            <Nav.Link className="navbarMenu mx-3" href="#" style={{ fontSize: "1.2rem" }}>
+            <Nav.Link className="navbarMenu mx-3 " href="#" style={{ fontSize: "1.2rem", fontWeight: "300"  }}>
              Servizi
             </Nav.Link>
             <Nav.Link
-              className="navbarMenu text-white mx-3"
+              className="navbarMenu mx-3"
               onClick={() => navigate("/ourbrand")}
-              style={{ fontSize: "1.2rem" }}
+              style={{ fontSize: "1.2rem", fontWeight: "300"  }}
             >
               Prodotti
             </Nav.Link>
           </Nav>
-          <Button className=" rounded-0 px-4" onClick={() => navigate("/")}>
+          <Button className="accedi-btn rounded-4 px-4" onClick={() => navigate("/")}>
             Accedi
           </Button>
-          <Button className="rounded-0 px-4 ms-3" onClick={() => navigate("/register")}>
+          <Button className=" registrati-btn rounded-4 px-4 ms-3" onClick={() => navigate("/register")}>
             Registrati
           </Button>
         </Navbar.Collapse>
--- src/components/ProfileSection/ProfileSection.tsx
+++ src/components/ProfileSection/ProfileSection.tsx
@@ -1,9 +1,35 @@
 
+import { Col, Container, Row } from 'react-bootstrap'
 import ProfileNav from './ProfileNav'
+import "./ProfileSection.css"
 
 const ProfileSection = () => {
   return (
+    <>
+    <Container fluid className='homePage text-center m-0 pt-4 px-5'>
     <ProfileNav/>
+      <h1>Gestisci il tuo salone a portata di click</h1>
+      <Container className='w-75 my-5'>
+      <h5 style={{fontWeight: "300"}}>Organizza facilmente gli appuntamenti del tuo salone con un'interfaccia intuitiva. Gestisci clienti, trattamenti e staff in pochi clic. Ottimizza il tempo e migliora l'efficienza del tuo business.</h5>
+    </Container>
+    <Container className='mx-auto mt-5'>
+      <Row className='justify-content-center gap-5'> 
+        <Col xs={12} md={3} className='first-card text-start rounded-4 p-4'>
+          <h3 className='mb-4'>Gestione Storico Appuntamenti</h3>
+          <h6 style={{fontWeight: "300"}}>Gestisci facilmente gli appuntamenti del salone, per assicurarti di non fare alcun ritardo.</h6>
+        </Col>
+        <Col xs={12} md={3} className='second-card text-start rounded-4 p-4'>
+          <h3 className='mb-5'>Gestione Storico Clienti</h3>
+          <h6 className="mt-5" style={{fontWeight: "300"}}>Gestisci facilmente i clienti e tutti i lori bisogni, per evitare di sbagliare.</h6>
+        </Col>
+        <Col xs={12} md={3} className='third-card text-start rounded-4 p-4'>
+          <h3 className='mb-4'>Piani di gestione personalizzati</h3>
+          <h6 className="mt-5" style={{fontWeight: "300"}}>Personalizza su misura il piano per ogni tuo bisogno.</h6>
+        </Col>
+      </Row>
+    </Container>
+    </Container>
+    </>
   )
 }
 
