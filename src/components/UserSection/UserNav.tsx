5fc5f6d691dc4aaf4741477a703193250e2e7258
ageria20 <ageria20@gmail.com>
2024-10-21 14:23:55 +0200

    adding avatar

--- src/components/UserSection/UserNav.tsx
+++ src/components/UserSection/UserNav.tsx
@@ -7,6 +7,7 @@ import { useAppDispatch, useAppSelector } from '../../redux/store/store'
 import { useEffect } from 'react'
 import { getClientMe } from '../../redux/actions/usersAction'
 import "./UserPage.css"
+import { PencilFill } from 'react-bootstrap-icons'
 
 const ProfileNav = () => {
 const navigate = useNavigate()
@@ -19,23 +20,24 @@ useEffect(() => {
 }, [dispatch])
 
   return (
-    <Navbar expand="lg" className="navbar nav mx-auto p-0 my-5">
-      <Container className="bg-white rounded-4 mb-5 d-flex justify-content-between">
+    <Navbar expand="lg" className="navbar nav mx-auto p-0 my-3">
+      <Container className="bg-white rounded-4 mb-5 d-flex justify-content-between flex-nowrap">
         <Navbar.Brand onClick={() => navigate("/")}>
           <Image src={nagefyLogo} alt="nagefy_logo" width={120} />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav" >
+        <Navbar.Collapse id="basic-navbar-nav ">
           
-          <Dropdown>
+          <Dropdown className='ms-auto d-flex justify-content-end'>
             <Dropdown.Toggle id="dropdown-basic" className="bg-transparent text-black border-0 px-4">
-            {loggedUser && `${loggedUser.name} ${loggedUser.surname}`}
+            {loggedUser && <Image src={loggedUser.avatar} width={50} className='rounded-circle'/>}
+            {/* {" "}{loggedUser && `${loggedUser.name} ${loggedUser.surname}`} */}
             </Dropdown.Toggle>
 
             <Dropdown.Menu className='custom-dropdown'>
               
               <Dropdown.Item className='text-white custom-dropdown-item' onClick={() => navigate("/edit-client")}>
-                Edit
+              <span><PencilFill/>{" "}</span> Edit
               </Dropdown.Item>
 
              
@@ -43,7 +45,7 @@ useEffect(() => {
                 navigate("/login-client")
                 localStorage.removeItem("accessToken")
                 }}>
-                <span><IoLogOut/></span>Logout
+                <span><IoLogOut/>{" "}</span> Logout
               </Dropdown.Item>
             </Dropdown.Menu>
           </Dropdown>
