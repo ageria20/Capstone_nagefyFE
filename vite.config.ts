0620d78a4d49ee32ce1469eeb56b5d12ab883bfb
ageria20 <ageria20@gmail.com>
2024-10-31 17:47:09 +0100

    adding nagefy favicon

--- vite.config.ts
+++ vite.config.ts
@@ -18,6 +18,32 @@ export default defineConfig({
       short_name: 'Nagefy',
       description: 'management saloon',
       theme_color: '#ffffff',
+      "icons": [
+    {
+      "src": "/pwa-192x192.png",
+      "sizes": "192x192",
+      "type": "image/png",
+      "purpose": "any"
+    },
+    {
+      "src": "/pwa-512x512.png",
+      "sizes": "512x512",
+      "type": "image/png",
+      "purpose": "any"
+    },
+    {
+      "src": "/pwa-maskable-192x192.png",
+      "sizes": "192x192",
+      "type": "image/png",
+      "purpose": "maskable"
+    },
+    {
+      "src": "/pwa-maskable-512x512.png",
+      "sizes": "512x512",
+      "type": "image/png",
+      "purpose": "maskable"
+    }
+  ],
     },
 
     workbox: {
